3d3d03fca9fb06bb14dfd0db87e7f8874de59b24
ASUS VivoBook <ASUS VivoBook@DESKTOP-LIARBAQ>
2024-03-09 09:29:44 -0600

    crud de AUTORES

--- PARCIAL1A/Controllers/AutoresControler.cs
+++ PARCIAL1A/Controllers/AutoresControler.cs
@@ -24,32 +24,9 @@ namespace PARCIAL1A.Controllers
         [Route("GetAll")]
         public IActionResult Get()
         {
-            var listadoAutores = (from e in _equiposContext.equipos
-                                 join t in _equiposContext.tipo_equipo
-                                 on e.tipo_equipo_id equals t.id_tipo_equipo
-                                 join m in _equiposContext.marcas
-                                 on e.marca_id equals m.id_marcas
-                                 join es in _equiposContext.estados_equipo
-                                 on e.estado_equipo_id equals es.id_estados_equipo
-                                 select new
-                                 {
-                                     e.id_equipos,
-                                     e.nombre,
-                                     e.descripcion,
-                                     e.tipo_equipo_id,
-                                     tipo_equipo = t.descripcion,
-                                     e.marca_id,
-                                     marca = m.nombre_marca,
-                                     e.estado_equipo_id,
-                                     estado_equipo = es.descripcion,
-                                     detalle = $"Tipo : {t.descripcion}, Marca {m.nombre_marca}, Estado equipo {es.descripcion}",
-                                     e.estado
-                                 }
-                                           ).OrderBy(resultado => resultado.estado_equipo_id)
-                                           .ThenBy(resultado => resultado.marca_id)
-                                           .ThenByDescending(resultado => resultado.tipo_equipo_id)
-                                           .ToList();
-
+            var listadoAutores = (from a in _parcial1AContext.Autores
+                                  select a).ToList();
+                                
 
             if (listadoAutores.Count() == 0)
             {
@@ -64,7 +41,7 @@ namespace PARCIAL1A.Controllers
         public IActionResult Get(int id)
         {
             Autores? autores = (from a in _parcial1AContext.Autores
-                               where a.Nombre == Nombre
+                               where a.Id == id
                                select a).FirstOrDefault();
 
             if (autores== null)
@@ -74,40 +51,32 @@ namespace PARCIAL1A.Controllers
             return Ok(autores);
         }
 
-        /// <sumary>
-        /// EndPoint que retorna el listado de todos los equipos existentes
-        /// </sumary>
-        /// <param name="id"></param>>
-        /// <returns></returns>
+       
         [HttpGet]
         [Route("Find/{filtro}")]
         public IActionResult FindByDescription(string filtro)
         {
-            equipos? equipo = (from e in _equiposContext.equipos
-                               where e.descripcion.Contains(filtro)
-                               select e).FirstOrDefault();
+            Autores? autores = (from a in _parcial1AContext.Autores
+                               where a.Nombre.Contains(filtro)
+                               select a).FirstOrDefault();
 
-            if (equipo == null)
+            if (autores == null)
             {
                 return NotFound();
             }
-            return Ok(equipo);
+            return Ok(autores);
         }
 
-        /// <sumary>
-        /// EndPoint que retorna el listado de todos los equipos existentes
-        /// </sumary>
-        /// <param name="id"></param>>
-        /// <returns></returns>
+        
         [HttpPost]
         [Route("Add")]
-        public IActionResult GuardarEquipo([FromBody] equipos equipo)
+        public IActionResult GuardarEquipo([FromBody] Autores autores)
         {
             try
             {
-                _equiposContext.equipos.Add(equipo);
-                _equiposContext.SaveChanges();
-                return Ok(equipo);
+                _parcial1AContext.Autores.Add(autores);
+                _parcial1AContext.SaveChanges();
+                return Ok(autores);
             }
             catch (Exception ex)
             {
@@ -117,27 +86,24 @@ namespace PARCIAL1A.Controllers
 
         [HttpPut]
         [Route("actualizar/{id}")]
-        public IActionResult ActualizarEquipo(int id, [FromBody] equipos equipoModificar)
+        public IActionResult ActualizarEquipo(int id, [FromBody] Autores autoresModificar)
         {
             //Para actualizar un registro, se obtiene el registro original de la base de datos
             //al cual alteraremos alguna propiedad
-            equipos? equipoActual = (from e in _equiposContext.equipos
-                                     where e.id_equipos == id
+            Autores? AutoresActual = (from e in _parcial1AContext.Autores
+                                     where e.Id == id
                                      select e).FirstOrDefault();
             //Verificamos que exista el registro segun su ID
-            if (equipoActual == null)
+            if (AutoresActual == null)
             { return NotFound(); }
             //Si se encuentra el registro, se alteran los campos modificables
-            equipoActual.nombre = equipoModificar.nombre;
-            equipoActual.descripcion = equipoModificar.descripcion;
-            equipoActual.marca_id = equipoModificar.marca_id;
-            equipoActual.tipo_equipo_id = equipoModificar.tipo_equipo_id;
-            equipoActual.anio_compra = equipoModificar.anio_compra;
-            equipoActual.costo = equipoModificar.costo;
+            AutoresActual.Nombre = autoresModificar.Nombre;
+
+
             //Se marca el registro como modificado en el contexto //y se envia la modificacion a la base de datos
-            _equiposContext.Entry(equipoActual).State = EntityState.Modified;
-            _equiposContext.SaveChanges();
-            return Ok(equipoModificar);
+            _parcial1AContext.Entry(AutoresActual).State = EntityState.Modified;
+            _parcial1AContext.SaveChanges();
+            return Ok(autoresModificar);
 
         }
 
@@ -148,16 +114,16 @@ namespace PARCIAL1A.Controllers
         {
             //Para actualizar un registro, se obtiene el registro original de la base de datos
             //al cual eliminaremos
-            equipos? equipo = (from e in _equiposContext.equipos
-                               where e.id_equipos == id
+            Autores? autores = (from e in _parcial1AContext.Autores
+                               where e.Id == id
                                select e).FirstOrDefault();
             //Verificamos que exista el registro segun su ID
-            if (equipo == null)
+            if (autores == null)
                 return NotFound();
             //Ejecutamos la accion de elminar el registro _equiposContexto.equipos.Attach(equipo);
-            _equiposContext.equipos.Remove(equipo);
-            _equiposContext.SaveChanges();
-            return Ok(equipo);
+            _parcial1AContext.Autores.Remove(autores);
+            _parcial1AContext.SaveChanges();
+            return Ok(autores);
         }
     }
 }
